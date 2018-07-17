9ee2baf46e912241dc846da6d38a0c346b11755d
hpreston <hank.preston@gmail.com>
2018-07-17 10:44:49 -0400

    YANG examples

---
+++ data_manipulation/yang/pyang-examples.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+# These example bash commands show how to use the pyang utility to work
+# with yang module files.
+#
+# Copyright (c) 2018 Cisco and/or its affiliates.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+echo "Print the YANG module in a simple text tree"
+pyang -f tree ietf-interfaces.yang
+
+echo "Print only part of the tree"
+pyang -f tree --tree-path=/interfaces/interface \
+  ietf-interfaces.yang
+
+echo "Print an example XML skeleton (NETCONF)"
+pyang -f sample-xml-skeleton ietf-interfaces.yang
+
+echo "Create an HTTP/JS view of the YANG Model"
+pyang -f jstree -o ietf-interfaces.html \
+  ietf-interfaces.yang
+open ietf-interfaces.html
+
+echo 'Control the "nested depth" in trees'
+pyang -f tree --tree-depth=2 ietf-ip.yang
+
+echo "Include deviation models in the processing"
+pyang -f tree \
+  --deviation-module=cisco-xe-ietf-ip-deviation.yang \
+  ietf-ip.yang
